640239b3a95e5c316397b435ea5f5109caf52ba0
Shubham1204pandey <125333556+Shubham1204pandey@users.noreply.github.com>
2024-02-26 15:26:53 +0530

    start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull shubham1204pandey/python-helloworld
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 shubham1204pandey/python-helloworld
